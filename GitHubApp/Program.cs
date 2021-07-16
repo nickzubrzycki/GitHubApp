a6551551446619c6ecaa6643be79b3d99924578a
Nick Zubrzycki <nick.zubrzycki@skynet.be>
2021-07-16 11:30:19 +0200

    Add project files.

---
+++ GitHubApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitHubApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Leeftijd: ");
+            int leeftijd = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
